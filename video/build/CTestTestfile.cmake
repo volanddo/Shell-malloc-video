e0419a35e155ed219c25c139cf3b7df1e3f33ca4
Dorian Voland <dorian.voland@gmail.com>
2023-02-21 20:39:07 +0100

    aad

--- video/build/CTestTestfile.cmake
+++ video/build/CTestTestfile.cmake
@@ -1,8 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: /user/7/volandd/Documents/2A/SEPC/ensimag-video
-# Build directory: /user/7/volandd/Documents/2A/SEPC/ensimag-video/build
+# Source directory: /Users/volanddorian/Downloads/1A2Aproject/1A/1A-S2/githubFolder/Shell-malloc-video/video
+# Build directory: /Users/volanddorian/Downloads/1A2Aproject/1A/1A-S2/githubFolder/Shell-malloc-video/video/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(CommonPthreadError "perl" "/user/7/volandd/Documents/2A/SEPC/ensimag-video/tests/pthcocci.pl")
-set_tests_properties(CommonPthreadError PROPERTIES  _BACKTRACE_TRIPLES "/user/7/volandd/Documents/2A/SEPC/ensimag-video/CMakeLists.txt;54;add_test;/user/7/volandd/Documents/2A/SEPC/ensimag-video/CMakeLists.txt;0;")
+add_test(CommonPthreadError "perl" "/Users/volanddorian/Downloads/1A2Aproject/1A/1A-S2/githubFolder/Shell-malloc-video/video/tests/pthcocci.pl")
+set_tests_properties(CommonPthreadError PROPERTIES  _BACKTRACE_TRIPLES "/Users/volanddorian/Downloads/1A2Aproject/1A/1A-S2/githubFolder/Shell-malloc-video/video/CMakeLists.txt;54;add_test;/Users/volanddorian/Downloads/1A2Aproject/1A/1A-S2/githubFolder/Shell-malloc-video/video/CMakeLists.txt;0;")
